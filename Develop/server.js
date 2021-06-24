df172c714c821cb19e4d6cd850978e8fb3ece917
Kevin Crawford <crawfordkevin101@gmail.com>
2021-06-24 18:48:05 -0400

    troubleshoot PORT issue

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const htmlRoutes = require('./Routes/html');
 const routesApi = require('./Routes/api');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
